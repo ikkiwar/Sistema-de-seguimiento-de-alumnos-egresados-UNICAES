e1c297d7ad456e33ff3feaaebb93281cb88461fd
Esperanza Duenas <esperanza.duenas@unplug.studio>
2020-02-24 22:03:25 -0600

    Clase formateada

--- unicaes/src/Components/Tabla.js
+++ unicaes/src/Components/Tabla.js
@@ -5,55 +5,45 @@ import '../Components/Styles/Tabla.css';
 import Api from '../Api';
 
 class Tabla extends React.Component {
+  /**
+  * Sorry si te da conflicto pero mi trastorno obsesivo compulsivo 
+  * tenia que ordenar el codigo :'v
+  */
+  constructor(props) {
+    // contructor para poder pasarle las props a nuestro Componente
+    super(props);
+      this.state = {
+        posts: []
+      };
+  }
 
+  componentDidMount() {
+    // este es un estado del ciclo de via de React se ejecuta cuando se carga la pagina
+    fetch(`${Api}${this.props.entidad}`, {
+      // aqui le pasamos el URL y la entidad que vamos a comsumir
+      method: "GET"
+    })
+    .then(Response => Response.json())
+    .then(posts => {
+      this.setState({
+        posts: posts
+        });
+    });
+  }
 
-     constructor(props) {
-         // contructor para poder pasarle las props a nuestro Componente
-         super(props);
-         this.state = {
-           posts: []
-         };
-       }
-     
-       componentDidMount() {
-         // este es un estado del ciclo de via de React se ejecuta cuando se carga la pagina
-         fetch(`${Api}${this.props.entidad}`, {
-           // aqui le pasamos el URL y la entidad que vamos a comsumir
-           method: "GET"
-         })
-           .then(Response => Response.json())
-           .then(posts => {
-             this.setState({
-               posts: posts
-               
-             });
-           });
-           
-       } 
-
-    render() {
-      console.log(this.state.posts);
-        return (
-            <div className="tabla">
-             <ReactTable
-                    columns={this.props.columnas}
-                    data={this.state.posts}
-                    filterable
-                    noDataText={"Datos No encontrados"}
-                    defaultPageSize={5}
-                    className="datos"
-                >
- 
-                </ReactTable>
-              
-
-            </div>
-
-
-        )
-
-    }
-
-
+  render() {
+    return (
+      <div className="tabla">
+        <ReactTable
+          columns={this.props.columnas}
+          data={this.state.posts}
+          filterable
+          noDataText={"Datos No encontrados"}
+          defaultPageSize={5}
+          className="datos"
+        />
+      </div>
+    )
+  }
 }
 export default Tabla;
