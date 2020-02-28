978b7385501214a3336b53a02096575a87f134b4
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-02-28 02:21:18 -0600

    formularios con combobox funcionando

--- unicaes/src/Components/Fdecanos.js
+++ unicaes/src/Components/Fdecanos.js
@@ -8,8 +8,38 @@ class Fdecanos extends React.Component{
         super(props);
         this.state = {
             facultades: [],
+            facultad: '',
+            dui: ''
          } 
        }
+
+       handleSubmit = async e => {
+        e.preventDefault()
+        console.log(this.state)
+
+
+        try {
+            let confing = {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+
+                body: JSON.stringify(this.state)
+            }
+
+            let res = await fetch(`${Api}${'/decanos'}`, confing)
+            let json = await res.json()
+
+            console.log(json)
+        } catch (error) {
+
+        }
+
+        //  window.location.replace('');
+
+    }
 
      componentDidMount() {
         fetch(`${Api}${"/facultades"}`, {
@@ -23,9 +53,16 @@ class Fdecanos extends React.Component{
         });
       }
 
+      handleChange = e => {
+        // console.log(`${e.target.name}: ${e.target.value}`)
+        let partialState = {}
+        partialState[e.target.name] = e.target.value
+        this.setState(partialState)
+    }
+
       handleSelectChange = (selectedValue) =>{
         this.setState({
-          selectedValue: selectedValue
+          facultad: selectedValue
         });
       }
 
@@ -50,7 +87,7 @@ return(
         <div class="col-4"></div>
     </div>
     <br />
-    <form name="nuevoDecano" method="POST">
+    <form onSubmit={this.handleSubmit}>
         <div class="container">
             <div class="container-fluid row d-flex p-2 bd-highlight border border-warning">
                 <div class="col-1"></div>
@@ -58,7 +95,8 @@ return(
                     <label for="txtDUI">DUI:</label>
                     <br />
                     <input type="text" class="form-control" name="txtDUI" 
-                    placeholder="Ej. 12345678-9" />
+                    placeholder="Ej. 12345678-9" onChange={this.handleChange}
+                    value={this.state.dui} name="dui" />
                 </div>
                 <div class="col-4">
                     <label for="cmbFacultad">Facultad:</label>
